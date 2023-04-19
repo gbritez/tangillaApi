93d28475ac23b80d2be4292427047658fe8ee377
Gonzalo <gonzalo_britez@hotmail.com>
2023-04-19 15:56:18 -0300

    cambio en el  reducer a async

--- core.js
+++ core.js
@@ -16,25 +16,15 @@ function removeDuplicatedResults(synset) {
     return synonyms.filter((item, index) => synonyms.indexOf(item) === index)
 }
 
-async function getWordnetSynonyms(word) {
-    const synonyms = await wordnet.lookupAsync(word);
-    return synonyms;
-}
-
 async function findSynonyms(text) {
     try {
         const words = cleanText(text);
 
-        let synonyms = {};
-        for (let word of words) {
-            let synset = await getWordnetSynonyms(word);
-            synonyms[word] = removeDuplicatedResults(synset)
-        }
-
-        const result = words.reduce((output, word) => {
-            const synonymsOfWord = synonyms[word]
-            const key = Object.keys(output).find(word => synonymsOfWord.includes(word));
-            return key ? { ...output, [key]: output[key] + 1 } : { ...output, [word]: 0 }
+        const result = await words.reduce(async (output, word) => {
+            const synonymsOfWord = await wordnet.lookupAsync(word).then((result) => { return removeDuplicatedResults(result) });
+            const synonyms = await output;
+            const key = Object.keys(synonyms).find(word => synonymsOfWord.includes(word));
+            return key ? { ...synonyms, [key]: synonyms[key] + 1 } : { ...synonyms, [word]: 0 }
         }, {})
 
         return Object.entries(result).map(([word, synonyms_found]) => ({ word, synonyms_found }))
